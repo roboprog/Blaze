65945b746966baabb6eca2ad0e69efac2fd742f6
Rob Anderson <roboprog@yahoo.com>
2009-12-28 07:14:54 -0800

    add license template to other C files

--- Blaze/symtab.c
+++ Blaze/symtab.c
@@ -2,6 +2,28 @@
  * Maintain the global and current context symbol table(s).
  */
 
+/*
+    blaze: mongrel language mashing perl and pascal concepts (among others)
+
+    Copyright (C) 2009, Robin R Anderson
+    roboprog@yahoo.com
+    PO 1608
+    Shingle Springs, CA 95682
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // *** import ****
 
 #include <stdlib.h>
